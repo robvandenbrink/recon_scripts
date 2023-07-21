31753e2ea74743e0c242acdf198c444942987091
Rob VandenBrink <rob@coherentsecurity.com>
2023-07-20 21:02:55 -0400

    Query Cisco Umbrella Passive DNS

--- umb.cmd
+++ umb.cmd
@@ -1,2 +1,2 @@
-@echo off
+@echo off 
 curl -s -k "https://investigate.api.umbrella.com//pdns/ip/%1" -H "Authorization: Bearer %umb-api-key%" -H "Content-Type: application/json"  | jq   | grep rr  | sed -e s/\"rr\"://g | tr -d \,\"" "
